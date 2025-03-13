4dfe8e5dffbd2c61e56608da7b65eed55f74c24e
zyaireD <zyaire.dowe25@compscihigh.org>
2025-03-13 23:59:14 +0000

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // import your mongoose
-
+const mongoose = require("mongoose");
 // install your mongoose
 
 const app = express();
@@ -11,11 +11,15 @@ app.use((req, res, next) => {
 });
 
 // create a studentSchema with name, grade, advisory, and fav subject
-
+const studentSchema = new mongoose.Schema({
+  name:{type:String, required :true},
+  grade:{type: Number, default: 9},
+  favsub:{type: String }
+})
 // connect your schema to a model called Student
 
 // create a route hanlder for /g12 that returns every student in grade 12
-
+const Student= mongoose.model("student", studentSchema)
 // (OYO) create a route hanlder for /me that returns yourself without using your name in the query
 
 // (OYO) create a route hanlder for /friend that returns someone at your table using their name in the query
@@ -27,7 +31,29 @@ app.use((req, res, next) => {
 // (OYO) save 2 more documents about students at your table
 // make sure to start your server 
 
+async function startsever(){
+  await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.2yqbx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  
+  const user1 = await new Student({
+    name: "zyaire",
+    grade:12,
+    favSub:"cs",
+   }).save()
+   
 
+  const user2 = await new Student({
+    name: "jayson",
+    grade:12,
+    favSub:"cs",
+   }).save()
+
+   app.listen(3000, () => {
+    console.log(`Server running`);
+  });
+   
+
+}
 // call startServer
-
+startsever();
 // if you finished all the excersizes try these 
+ 
